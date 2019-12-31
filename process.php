fe92497dc6af5764e3cb45762363f637f6d8aba9
daniel-12 <51939760+daniel-12@users.noreply.github.com>
2019-12-30 22:49:10 -0800

    Add files via upload

--- process.php
+++ process.php
@@ -2,14 +2,14 @@
   if (isset($_POST['email']))  {
   
     //Email information
-    $admin_email = "danielrgueta28@gmail.com";
+    $admin_email = "danidose29@gmail.com";
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
     
     //send email
-    mail($admin_email, "New Print Submission", $message . ' - ' . $phone, "From:" . $email);
+    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
     header('Location: http://edgeledger.net/success.html');
   }
